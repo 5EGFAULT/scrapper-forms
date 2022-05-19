21289616a0e9c5576d1f720ab9cb61e7c3d54c24
Souinia Ayoub <souiniaayoub@gmail.com>
2022-05-19 15:29:30 +0100

    lst ext .....

--- src/Loadboard.tsx
+++ src/Loadboard.tsx
@@ -1,9 +1,346 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import { MultiSelect } from "react-multi-select-component";
 
-type Props = {}
+const options = [
+  { value: "AL", label: "Alabama" },
+  { value: "AK", label: "Alaska" },
+  { value: "AS", label: "American Samoa" },
+  { value: "AZ", label: "Arizona" },
+  { value: "AR", label: "Arkansas" },
+  { value: "CA", label: "California" },
+  { value: "CO", label: "Colorado" },
+  { value: "CT", label: "Connecticut" },
+  { value: "DE", label: "Delaware" },
+  { value: "DC", label: "District Of Columbia" },
+  { value: "FM", label: "Federated States Of Micronesia" },
+  { value: "FL", label: "Florida" },
+  { value: "GA", label: "Georgia" },
+  { value: "GU", label: "Guam" },
+  { value: "HI", label: "Hawaii" },
+  { value: "ID", label: "Idaho" },
+  { value: "IL", label: "Illinois" },
+  { value: "IN", label: "Indiana" },
+  { value: "IA", label: "Iowa" },
+  { value: "KS", label: "Kansas" },
+  { value: "KY", label: "Kentucky" },
+  { value: "LA", label: "Louisiana" },
+  { value: "ME", label: "Maine" },
+  { value: "MH", label: "Marshall Islands" },
+  { value: "MD", label: "Maryland" },
+  { value: "MA", label: "Massachusetts" },
+  { value: "MI", label: "Michigan" },
+  { value: "MN", label: "Minnesota" },
+  { value: "MS", label: "Mississippi" },
+  { value: "MO", label: "Missouri" },
+  { value: "MT", label: "Montana" },
+  { value: "NE", label: "Nebraska" },
+  { value: "NV", label: "Nevada" },
+  { value: "NH", label: "New Hampshire" },
+  { value: "NJ", label: "New Jersey" },
+  { value: "NM", label: "New Mexico" },
+  { value: "NY", label: "New York" },
+  { value: "NC", label: "North Carolina" },
+  { value: "ND", label: "North Dakota" },
+  { value: "MP", label: "Northern Mariana Islands" },
+  { value: "OH", label: "Ohio" },
+  { value: "OK", label: "Oklahoma" },
+  { value: "OR", label: "Oregon" },
+  { value: "PW", label: "Palau" },
+  { value: "PA", label: "Pennsylvania" },
+  { value: "PR", label: "Puerto Rico" },
+  { value: "RI", label: "Rhode Island" },
+  { value: "SC", label: "South Carolina" },
+  { value: "SD", label: "South Dakota" },
+  { value: "TN", label: "Tennessee" },
+  { value: "TX", label: "Texas" },
+  { value: "UT", label: "Utah" },
+  { value: "VT", label: "Vermont" },
+  { value: "VI", label: "Virgin Islands" },
+  { value: "VA", label: "Virginia" },
+  { value: "WA", label: "Washington" },
+  { value: "WV", label: "West Virginia" },
+  { value: "WI", label: "Wisconsin" },
+  { value: "WY", label: "Wyoming" },
+];
 
-export default function Loadboard({}: Props) {
+type formdata = {
+  origin: null | string;
+  origin_dh: null | string;
+  destination: null | string;
+  destination_dh: null | string;
+  general_specifc: null | string;
+  truck_type_general: null | string;
+  truck_type_specific: string[];
+  length: null | string;
+  weight: null | string;
+  full_partial: null | string;
+  startDate: null | string;
+  endDate: null | string;
+};
+
+export default function TruckersEdge() {
+  const [showSpec, setshowSpec] = useState<0 | 1 | 2>(0);
+  const [selected, setSelected] = useState<{ value: string; label: string }[]>(
+    []
+  );
+  const [formData, setFormData] = useState<formdata>({
+    origin: null,
+    origin_dh: null,
+    destination: null,
+    destination_dh: null,
+    general_specifc: null,
+    truck_type_general: null,
+    truck_type_specific: [],
+    length: null,
+    weight: null,
+    full_partial: null,
+    startDate: null,
+    endDate: null,
+  });
+
+  const formref = useRef<HTMLFormElement | null>(null);
+  const resetform = () => {
+    if (formref.current) {
+      formref.current.reset();
+      setFormData({
+        origin: null,
+        origin_dh: null,
+        destination: null,
+        destination_dh: null,
+        general_specifc: null,
+        truck_type_general: null,
+        truck_type_specific: [],
+        length: null,
+        weight: null,
+        full_partial: null,
+        startDate: null,
+        endDate: null,
+      });
+    }
+  };
+  useEffect(() => {
+    setFormData({
+      ...formData,
+      truck_type_specific: selected.map((v) => v.value),
+    });
+  }, [selected]);
+
+  const submitform = () => {
+    console.log(formData);
+    //if (formref.current) {
+    //  formref.current.reset();
+    //  setFormData({
+    //    origin: null,
+    //    origin_dh: null,
+    //    destination: null,
+    //    destination_dh: null,
+    //    general_specifc: null,
+    //    truck_type_general: null,
+    //    truck_type_specific: [],
+    //    length: null,
+    //    weight: null,
+    //    full_partial: null,
+    //    startDate: null,
+    //    endDate: null,
+    //  });
+    //}
+  };
+
   return (
-	<form>Loadboard</form>
-  )
+    <form className="w-full	 bg-white  rounded-lg" ref={formref}>
+      <h2 className="text-center text-2xl">Truckers Edge </h2>
+      <div className="flex w-full justify-between gap-3 flex-wrap">
+        <label className="flex flex-col my-2 flex-grow">
+          <span className="text-black mb-2 text-base capitalize">Origin:</span>
+          <input
+            onChange={(e) => {
+              setFormData({ ...formData, origin: e.target.value });
+            }}
+            type="text"
+            className="rounded-md text-base"
+            placeholder="Origin"
+          />
+        </label>
+        <label className="flex flex-col my-2 w-full sm:max-w-[150px]">
+          <span className="text-black mb-2 text-base capitalize">Dh-O:</span>
+          <input
+            onChange={(e) => {
+              setFormData({ ...formData, origin_dh: e.target.value });
+            }}
+            type="number"
+            className="rounded-md text-base "
+            placeholder="Dh-O"
+          />
+        </label>
+      </div>
+      <div className="flex flex-wrap justify-evenly">
+        <label className="flex h-7 items-center">
+          <input
+            type="radio"
+            name="spec"
+            value="City"
+            onChange={(e) => {
+              setFormData({ ...formData, general_specifc: e.target.value });
+            }}
+            onClick={() => setshowSpec(0)}
+          />
+          <span className="ml-2">City</span>
+        </label>
+        <label className="flex h-7 items-center">
+          <input
+            type="radio"
+            name="spec"
+            onChange={(e) => {
+              setFormData({ ...formData, general_specifc: e.target.value });
+            }}
+            value="States"
+            onClick={() => setshowSpec(1)}
+          />
+          <span className="ml-2">States</span>
+        </label>
+        <label className="flex h-7 items-center">
+          <input
+            type="radio"
+            name="spec"
+            onChange={(e) => {
+              setFormData({ ...formData, general_specifc: e.target.value });
+            }}
+            value="Anywhere"
+            onClick={() => setshowSpec(2)}
+          />
+          <span className="ml-2">Anywhere</span>
+        </label>
+      </div>
+      <label className="flex my-2  flex-col flex-wrap justify-evenly">
+        {showSpec === 0 && (
+          <div className="flex w-full justify-between gap-3 flex-wrap">
+            <label className="flex flex-col my-2 flex-grow">
+              <span className="text-black mb-2 text-base capitalize">
+                Destination:
+              </span>
+              <input
+                onChange={(e) => {
+                  setFormData({ ...formData, destination: e.target.value });
+                }}
+                type="text"
+                className="rounded-md text-base"
+                placeholder="Destination"
+              />
+            </label>
+            <label className="flex flex-col my-2 w-full sm:max-w-[150px]">
+              <span className="text-black mb-2 text-base capitalize">
+                Dh-D:
+              </span>
+              <input
+                onChange={(e) => {
+                  setFormData({ ...formData, destination_dh: e.target.value });
+                }}
+                type="number"
+                className="rounded-md text-base "
+                placeholder="Dh-D"
+              />
+            </label>
+          </div>
+        )}
+        {showSpec === 1 && (
+          <MultiSelect
+            className="max-w-full"
+            options={options}
+            value={selected}
+            onChange={setSelected}
+            labelledBy={"Truck type"}
+          />
+        )}
+        {showSpec === 2 && (
+          <div className="">
+            <label className="flex flex-col my-2 flex-grow">
+              <span className=""></span>
+              <input type="checkbox" name="specdest" value="States" />
+            </label>
+          </div>
+        )}
+      </label>
+
+      <div className="flex w-full justify-between gap-3 flex-wrap">
+        <label className="flex flex-col my-2 flex-grow">
+          <span className="text-black mb-2 text-base capitalize">Length:</span>
+          <input
+            onChange={(e) => {
+              setFormData({ ...formData, length: e.target.value });
+            }}
+            type="number"
+            className="rounded-md text-base "
+            placeholder="Length"
+          />
+        </label>
+        <label className="flex flex-col my-2 flex-grow">
+          <span className="text-black mb-2 text-base capitalize">Weight:</span>
+          <input
+            onChange={(e) => {
+              setFormData({ ...formData, weight: e.target.value });
+            }}
+            type="number"
+            className="rounded-md text-base "
+            placeholder="Weight"
+          />
+        </label>
+      </div>
+      <label className="flex my-2  flex-col flex-wrap justify-evenly">
+        <span className="mb-2">Full/partial</span>
+        <select
+          className="rounded-md text-base"
+          onChange={(e) => {
+            setFormData({ ...formData, full_partial: e.target.value });
+          }}
+        >
+          <option value="Both">Both</option>
+          <option value="Full">Full</option>
+          <option value="Partial">Partial</option>
+        </select>
+      </label>
+      <div className="flex w-full justify-between gap-3 flex-wrap">
+        <label className="flex flex-col my-2 flex-grow">
+          <span className="text-black mb-2 text-base capitalize">
+            Start date:
+          </span>
+          <input
+            onChange={(e) => {
+              setFormData({ ...formData, startDate: e.target.value });
+            }}
+            type="date"
+            className="rounded-md text-base "
+            placeholder="Start date"
+          />
+        </label>
+        <label className="flex flex-col my-2 flex-grow">
+          <span className="text-black mb-2 text-base capitalize">
+            End date:
+          </span>
+          <input
+            onChange={(e) => {
+              setFormData({ ...formData, endDate: e.target.value });
+            }}
+            type="date"
+            className="rounded-md text-base "
+            placeholder="End date"
+          />
+        </label>
+      </div>
+
+      <div className="flex w-full justify-evenly flex-wrap gap-3">
+        <div
+          onClick={resetform}
+          className=" my-2 min-w-full sm:flex-grow sm:min-w-0 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Reset
+        </div>
+        <div
+          onClick={submitform}
+          className="  my-2 min-w-full sm:flex-grow sm:min-w-0 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </div>
+      </div>
+    </form>
+  );
 }
--- src/TruckersEdge.tsx
+++ src/TruckersEdge.tsx
@@ -1,27 +1,162 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { MultiSelect } from "react-multi-select-component";
 
 const options = [
-  { label: "Grapes 🍇", value: "grapes" },
-  { label: "Mango 🥭", value: "mango" },
-  { label: "Strawberry 🍓", value: "strawberry" },
-  { label: "Watermelon 🍉", value: "watermelon" },
-  { label: "Pear 🍐", value: "pear", disabled: true },
-  { label: "Apple 🍎", value: "apple" },
-  { label: "Tangerine 🍊", value: "tangerine" },
-  { label: "Pineapple 🍍", value: "pineapple" },
-  { label: "Peach 🍑", value: "peach" },
+  { value: "AC", label: "Auto Carrier" },
+  { value: "BT", label: "B-Train" },
+  { value: "CN", label: "Conestoga" },
+  { value: "C", label: "Container" },
+  { value: "CI", label: "Container Insulated" },
+  { value: "CR", label: "Container Refrigerated" },
+  { value: "CV", label: "Conveyor" },
+  { value: "DD", label: "Double Drop" },
+  { value: "LA", label: "Drop Deck Landoll" },
+  { value: "DT", label: "Dump Trailer" },
+  { value: "F", label: "Flatbed" },
+  { value: "FA", label: "Flatbed Air-Ride" },
+  { value: "FN", label: "Flatbed Conestoga" },
+  { value: "F2", label: "Flatbed Double" },
+  { value: "FZ", label: "Flatbed HazMat" },
+  { value: "FH", label: "Flatbed Hotshot" },
+  { value: "MX", label: "Flatbed Maxi" },
+  { value: "FD", label: "Flatbed or Step Deck" },
+  { value: "FS", label: "Flatbed w/Sides" },
+  { value: "FT", label: "Flatbed w/Tarps" },
+  { value: "FM", label: "Flatbed w/Team" },
+  { value: "FO", label: "Flatbed, Over Dimension" },
+  { value: "FC", label: "Flatbed w/Chains" },
+  { value: "FR", label: "Flatbed/Van/Reefer" },
+  { value: "HB", label: "Hopper Bottom" },
+  { value: "IR", label: "Insulated Van or Reefer" },
+  { value: "LB", label: "Lowbey" },
+  { value: "LR", label: "Lowbey or RGN" },
+  { value: "LO", label: "Lowbey, over Dimension" },
+  { value: "MV", label: "Moving Van" },
+  { value: "NU", label: "Pneumatic" },
+  { value: "PO", label: "Power only" },
+  { value: "R", label: "Reefer" },
+  { value: "RA", label: "Reefer Air-Ride" },
+  { value: "R2", label: "Reefer Double" },
+  { value: "RZ", label: "Reefer HazMat" },
+  { value: "RN", label: "Reefer Intermodal" },
+  { value: "RL", label: "Reefer Logistics" },
+  { value: "RV", label: "Reefer or Vented Van" },
+  { value: "RM", label: "Reefer w/Team" },
+  { value: "RM", label: "Reefer w/Pallet Exchange" },
+  { value: "RG", label: "Removable Gooseneck" },
+  { value: "SD", label: "Step Deck" },
+  { value: "SR", label: "Step Deck or RGN" },
+  { value: "SN", label: "StepDeck Conestoga" },
+  { value: "SB", label: "Straight Box Truck" },
+  { value: "ST", label: "Stretch Trailer" },
+  { value: "TA", label: "Tanker Aluminum" },
+  { value: "TN", label: "Tanker Intermodal" },
+  { value: "TS", label: "Tanker Steel" },
+  { value: "TT", label: "Tanker And Trailer" },
+  { value: "V", label: "Van" },
+  { value: "VA", label: "Van Air-Ride" },
+  { value: "VS", label: "Van Conestoga" },
+  { value: "V2", label: "Van Double" },
+  { value: "VZ", label: "Van HazMat" },
+  { value: "VH", label: "Van Hotshot" },
+  { value: "VI", label: "Van Insulated" },
+  { value: "VN", label: "Van Intermodal" },
+  { value: "VG", label: "Van Lift-Gate" },
+  { value: "VL", label: "Van Logistics" },
+  { value: "OT", label: "Van Open-top" },
+  { value: "VB", label: "Van Roller bed" },
+  { value: "V3", label: "Van Triple" },
+  { value: "VV", label: "Van Vented" },
+  { value: "VF", label: "Van or Flatbed" },
+  { value: "VT", label: "Van or Flatbed w/Tarps" },
+  { value: "VR", label: "Van or Reefer" },
+  { value: "VC", label: "Van w/Curtains" },
+  { value: "VM", label: "Van w/Team" },
+  { value: "VW", label: "Van w/Blanket Wrap" },
+  { value: "VP", label: "Van w/Pallet Exchange" },
 ];
+
+type formdata = {
+  origin: null | string;
+  origin_dh: null | string;
+  destination: null | string;
+  destination_dh: null | string;
+  general_specifc: null | string;
+  truck_type_general: null | string;
+  truck_type_specific: string[];
+  length: null | string;
+  weight: null | string;
+  full_partial: null | string;
+  startDate: null | string;
+  endDate: null | string;
+};
 
 export default function TruckersEdge() {
   const [showSpec, setshowSpec] = useState(false);
-  const [selected, setSelected] = useState([]);
+  const [selected, setSelected] = useState<{ value: string; label: string }[]>(
+    []
+  );
+  const [formData, setFormData] = useState<formdata>({
+    origin: null,
+    origin_dh: null,
+    destination: null,
+    destination_dh: null,
+    general_specifc: null,
+    truck_type_general: null,
+    truck_type_specific: [],
+    length: null,
+    weight: null,
+    full_partial: null,
+    startDate: null,
+    endDate: null,
+  });
 
   const formref = useRef<HTMLFormElement | null>(null);
   const resetform = () => {
     if (formref.current) {
       formref.current.reset();
+      setFormData({
+        origin: null,
+        origin_dh: null,
+        destination: null,
+        destination_dh: null,
+        general_specifc: null,
+        truck_type_general: null,
+        truck_type_specific: [],
+        length: null,
+        weight: null,
+        full_partial: null,
+        startDate: null,
+        endDate: null,
+      });
     }
+  };
+  useEffect(() => {
+    setFormData({
+      ...formData,
+      truck_type_specific: selected.map((v) => v.value),
+    });
+  }, [selected]);
+
+  const submitform = () => {
+    console.log(formData);
+    //if (formref.current) {
+    //  formref.current.reset();
+    //  setFormData({
+    //    origin: null,
+    //    origin_dh: null,
+    //    destination: null,
+    //    destination_dh: null,
+    //    general_specifc: null,
+    //    truck_type_general: null,
+    //    truck_type_specific: [],
+    //    length: null,
+    //    weight: null,
+    //    full_partial: null,
+    //    startDate: null,
+    //    endDate: null,
+    //  });
+    //}
   };
 
   return (
@@ -31,6 +166,9 @@ export default function TruckersEdge() {
         <label className="flex flex-col my-2 flex-grow">
           <span className="text-black mb-2 text-base capitalize">Origin:</span>
           <input
+            onChange={(e) => {
+              setFormData({ ...formData, origin: e.target.value });
+            }}
             type="text"
             className="rounded-md text-base"
             placeholder="Origin"
@@ -39,6 +177,9 @@ export default function TruckersEdge() {
         <label className="flex flex-col my-2 w-full sm:max-w-[150px]">
           <span className="text-black mb-2 text-base capitalize">Dh-O:</span>
           <input
+            onChange={(e) => {
+              setFormData({ ...formData, origin_dh: e.target.value });
+            }}
             type="number"
             className="rounded-md text-base "
             placeholder="Dh-O"
@@ -51,6 +192,9 @@ export default function TruckersEdge() {
             Destination:
           </span>
           <input
+            onChange={(e) => {
+              setFormData({ ...formData, destination: e.target.value });
+            }}
             type="text"
             className="rounded-md text-base"
             placeholder="Destination"
@@ -59,6 +203,9 @@ export default function TruckersEdge() {
         <label className="flex flex-col my-2 w-full sm:max-w-[150px]">
           <span className="text-black mb-2 text-base capitalize">Dh-D:</span>
           <input
+            onChange={(e) => {
+              setFormData({ ...formData, destination_dh: e.target.value });
+            }}
             type="number"
             className="rounded-md text-base "
             placeholder="Dh-D"
@@ -71,6 +218,9 @@ export default function TruckersEdge() {
             type="radio"
             name="spec"
             value="General"
+            onChange={(e) => {
+              setFormData({ ...formData, general_specifc: e.target.value });
+            }}
             onClick={() => setshowSpec(false)}
           />
           <span className="ml-2">General</span>
@@ -79,6 +229,9 @@ export default function TruckersEdge() {
           <input
             type="radio"
             name="spec"
+            onChange={(e) => {
+              setFormData({ ...formData, general_specifc: e.target.value });
+            }}
             value="Specific"
             onClick={() => setshowSpec(true)}
           />
@@ -89,6 +242,7 @@ export default function TruckersEdge() {
         <span className="mb-2">Truck type</span>
         {showSpec && (
           <MultiSelect
+            className="max-w-full"
             options={options}
             value={selected}
             onChange={setSelected}
@@ -96,10 +250,23 @@ export default function TruckersEdge() {
           />
         )}
         {!showSpec && (
-          <select className="rounded-md text-base">
-            <option value="">Select1</option>
-            <option value="">Select2</option>
-            <option value="">Select3</option>
+          <select
+            className="rounded-md text-base"
+            onChange={(e) => {
+              setFormData({ ...formData, truck_type_general: e.target.value });
+            }}
+          >
+            <option value="V">Vans,Standard</option>
+            <option value="F">Flatbeds</option>
+            <option value="R">Reefers</option>
+            <option value="N">Conestoga</option>
+            <option value="C">Containers</option>
+            <option value="D">Decks,Standard</option>
+            <option value="B">Dry bulk</option>
+            <option value="Z">Hazardous materials</option>
+            <option value="O">Other equipement</option>
+            <option value="T">Tankers</option>
+            <option value="S">Vans Specialized</option>
           </select>
         )}
       </label>
@@ -107,6 +274,9 @@ export default function TruckersEdge() {
         <label className="flex flex-col my-2 flex-grow">
           <span className="text-black mb-2 text-base capitalize">Length:</span>
           <input
+            onChange={(e) => {
+              setFormData({ ...formData, length: e.target.value });
+            }}
             type="number"
             className="rounded-md text-base "
             placeholder="Length"
@@ -115,6 +285,9 @@ export default function TruckersEdge() {
         <label className="flex flex-col my-2 flex-grow">
           <span className="text-black mb-2 text-base capitalize">Weight:</span>
           <input
+            onChange={(e) => {
+              setFormData({ ...formData, weight: e.target.value });
+            }}
             type="number"
             className="rounded-md text-base "
             placeholder="Weight"
@@ -123,10 +296,15 @@ export default function TruckersEdge() {
       </div>
       <label className="flex my-2  flex-col flex-wrap justify-evenly">
         <span className="mb-2">Full/partial</span>
-        <select className="rounded-md text-base">
-          <option value="">full</option>
-          <option value="">Both</option>
-          <option value="">Select3</option>
+        <select
+          className="rounded-md text-base"
+          onChange={(e) => {
+            setFormData({ ...formData, full_partial: e.target.value });
+          }}
+        >
+          <option value="Both">Both</option>
+          <option value="Full">Full</option>
+          <option value="Partial">Partial</option>
         </select>
       </label>
       <div className="flex w-full justify-between gap-3 flex-wrap">
@@ -135,6 +313,9 @@ export default function TruckersEdge() {
             Start date:
           </span>
           <input
+            onChange={(e) => {
+              setFormData({ ...formData, startDate: e.target.value });
+            }}
             type="date"
             className="rounded-md text-base "
             placeholder="Start date"
@@ -145,6 +326,9 @@ export default function TruckersEdge() {
             End date:
           </span>
           <input
+            onChange={(e) => {
+              setFormData({ ...formData, endDate: e.target.value });
+            }}
             type="date"
             className="rounded-md text-base "
             placeholder="End date"
@@ -152,14 +336,17 @@ export default function TruckersEdge() {
         </label>
       </div>
 
-      <div className="flex w-full justify-evenly flex-wrap">
+      <div className="flex w-full justify-evenly flex-wrap gap-3">
         <div
           onClick={resetform}
-          className=" my-2 min-w-full sm:min-w-[200px] cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className=" my-2 min-w-full sm:flex-grow sm:min-w-0 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Reset
         </div>
-        <div className="  my-2 min-w-full sm:min-w-[200px] cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <div
+          onClick={submitform}
+          className="  my-2 min-w-full sm:flex-grow sm:min-w-0 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
           Submit
         </div>
       </div>
